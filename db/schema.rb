0cdb705592b5b9d8dbe1da4715cfd355ad27eb7f
jannilsonn <jannilsonn@gmail.com>
2025-07-21 13:16:10 -0300

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_07_21_000540) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "original_body", null: false
